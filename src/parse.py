150a7163285f5dc3081236e9dd977bf5f1fc9378
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-12-02 19:13:51 +0700

    Remove redundant file

--- src/parse.py
+++ src/parse.py
@@ -1,36 +1,160 @@
-import ast
+import os
+import hashlib
+from scalpel.cfg import CFGBuilder, CFG
+from scalpel.call_graph.pycg import CallGraphGenerator
 
-def parse(code):
-    tree = ast.parse(code)
-    parsed_data = []
 
-    for node in ast.walk(tree):
-        if isinstance(node, ast.FunctionDef):
-            parsed_data.append({
-                "type": "function",
-                "name": node.name,
-                "body": node.body
-            })
+def _get_folder_tree(path: str) -> dict:
+    '''
+    Create a folder tree from directory.
+    
+    Example result:
+    ```
+    {
+        "name": <path>,
+        "ext": ".",
+        "children": [
+            { "name": "file.py",    "ext": ".py"  },
+            { "name": "file.png",   "ext": ".png" },
+            { "name": "somefolder", "ext": ".", "children": [] }
+        ]
+    }
+    ```
+    '''
 
-        elif isinstance(node, ast.ClassDef):
-            parsed_data.append({
-                "type": "class",
-                "name": node.name,
-                "body": node.body
-            })
+    name = os.path.basename(path)
+    if os.path.isdir(path):
+        return {
+            "name": name,
+            "ext": ".",
+            "children": [
+                _get_folder_tree(os.path.join(path, child))
+                for child in os.listdir(path)
+            ]
+        }
+    else:
+        if "." not in name:
+            ext = ""
+        elif "." not in name[1:]:
+            ext = name
+        else:
+            _, ext = os.path.splitext(name)
+        return {
+            "name": name,
+            "ext": ext,
+            "children": []
+        }
 
-        elif isinstance(node, ast.Expr):
-            parsed_data.append({
-                "type": "expr",
-                "name": node.value,
-                "body": node.value
-            })
 
-        elif isinstance(node, ast.Assign):
-            parsed_data.append({
-                "type": "assign",
-                "targets": node.targets,
-                "value": node.value
-            })
+def _construct_pyfile_children(package: str, file: str) -> dict:
+    '''
+    Get all functions with their respective offset (first, last).
+    
+    Example result:
+    ```
+    {
+        "func_1": {
+            "id": "123",
+            "type": "function",
+            "first": 1,
+            "last": 6,
+            "children": {},
+        },
+        "class_2": {
+            "id": "456",
+            "type": "class",
+            "first": 7,
+            "last": 10,
+            "children": {
+                "__init__": {
+                    "id": "789",
+                    "type": "class:method",
+                    "first": 8,
+                    "last": 10,
+                    "children": {}
+                }
+            }
+        }
+    }
+    ```
+    '''
+    
+    cg_generator = CallGraphGenerator([file], package)
+    cg_generator.analyze()
 
-    return parsed_data
+    # exclude package from file name
+    name = file[len(package)+1:]
+
+    # exclude extension
+    name, _ = os.path.splitext(name)
+    name = name.replace('/', '.')
+
+    # extract offset dict
+    data = cg_generator.output_internal_mods()
+    data = data[name]['methods']
+
+    # file structure and id->obj mapping
+    structure, hashmap = {}, {}
+
+    def _build(structure, cfg, name, parent_type=''):
+        class_cfgs = cfg.class_cfgs.values()
+        function_cfgs = cfg.functioncfgs.values()
+
+        for cfg in class_cfgs:
+            full_name = name + '.' + cfg.name
+            id = hashlib.sha256(full_name.encode()).hexdigest()
+            structure[cfg.name] = {
+                'id': hashlib.sha256(full_name.encode()).hexdigest(),
+                'type': parent_type + ':class',
+                'first': data[full_name]['first'],
+                'last': data[full_name]['last'],
+                'children': {}
+            }
+            hashmap[id] = full_name
+            _build(
+                structure[cfg.name]['children'],
+                cfg,
+                full_name,
+                structure[cfg.name]['type']
+            )
+
+        for cfg in function_cfgs:
+            full_name = name + '.' + cfg.name
+            id = hashlib.sha256(full_name.encode()).hexdigest()
+            tpe = 'method' if parent_type.split(':')[-1] == 'class' else 'function'
+            structure[cfg.name] = {
+                'id': hashlib.sha256(full_name.encode()).hexdigest(),
+                'type': parent_type + ':' + tpe,
+                'first': data[full_name]['first'],
+                'last': data[full_name]['last'],
+                'children': {}
+            }
+            hashmap[id] = full_name
+            _build(
+                structure[cfg.name]['children'],
+                cfg,
+                full_name,
+                structure[cfg.name]['type']
+            )
+
+    _build(structure, CFGBuilder().build_from_file("", file), name)
+
+    return structure, hashmap
+
+
+def _include_pyfile_children(package: str, node: dict, path: str):
+    if node['ext'] == ".":
+        for child in node['children']:
+            _include_pyfile_children(package, child, os.path.join(path, child['name']))
+    elif node['ext'] == ".py" and node['name'] != '__init__.py':
+        node['children'], _ = _construct_pyfile_children(package, path)
+
+
+def get_full_structure(path: str):
+    '''
+    Same as _get_folder_tree, but now Python files
+    have their own children (functions, classes, methods, ...)
+    '''
+    res = _get_folder_tree(path)
+    _include_pyfile_children(path, res, path)
+    return res
